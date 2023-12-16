c2c84393af0a1c355361dcae4e0919f406b2b2eb
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-12-16 16:03:29 -0700

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import TodayWidget from './TodayWidget.vue';
+</script>
+
+<template>
+  <main class="grid">
+    <TodayWidget />
+    <div>
+      Widget 2
+    </div>
+    <div>
+      Widget 3
+    </div>
+  </main>
+</template>
+
+<style scoped>
+  .grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 1rem;
+  }
+</style>
