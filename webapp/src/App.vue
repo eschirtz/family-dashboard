579c98fc52ed1e74432f2855dfc002525c7e2a4b
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-12-17 00:26:50 -0700

    technically rendering

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 import { initializeApp } from "firebase/app"
@@ -16,15 +17,17 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const functions = getFunctions(app);
 
+interface Task {
+  title: string,
+  priority: number,
+  state?: string,
+}
+
+const tasks = ref<{ [id: string]: Task }>({})
 const getTasks = httpsCallable(functions, 'getTasks');
-getTasks()
-  .then((result) => {
-    // Read result of the Cloud Function.
-    /** @type {any} */
-    const payload = result.data;
-    console.log(payload);
-    
-  });
+getTasks().then((result) => {
+  tasks.value = result.data as {[id: string]: Task};        
+});
 
 </script>
 
@@ -33,8 +36,10 @@ getTasks()
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <HelloWorld msg="You did it!" />      
+      <div v-for="(task, id) in tasks" :key="id">
+        {{ task }}
+      </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
