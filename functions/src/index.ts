1db1f7867d84eceead141ed82275e360312636ac
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-12-26 16:14:55 -0600

    basic issue sync

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,67 +1,20 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onCall} from "firebase-functions/v2/https";
+import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
-import {LinearClient} from "@linear/sdk";
+import {initializeApp} from "firebase-admin/app";
+import {getFirestore} from "firebase-admin/firestore";
 
+initializeApp();
+const db = getFirestore();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+export const linearIssues = onRequest(async (req, res) => {
+  logger.info("issue changed");
+  const issue = req.body.data;
+  logger.info("issue", issue);
 
-export const getTasks = onCall(async () => {
-  logger.info("getTasks");
-  // Api key authentication
-  const linear = new LinearClient({
-    apiKey: process.env.LINEAR_API_KEY,
-  });
-  logger.info("linear initialization complete");
-  // const text = request.data.text;
-  // Authentication / user information is automatically added to the request.
-  // const uid = request.auth?.uid;
+  const id = issue.id;
+  logger.info("id", id);
 
-  // get linear issues
-  const me = await linear.viewer;
-  logger.info("linear viewer", me);
-  // const myIssues = await me.assignedIssues();
-
-  const issues = await linear.issues({
-    filter: {
-
-    },
-  });
-  const tasks: {[id: string]: {
-    title: string,
-    priority?: number,
-    state?: string
-  }} = {};
-
-  for (const issue of issues.nodes) {
-    logger.info("issue", issue);
-    const title = issue.title;
-    const priority = issue.priority;
-    const workflowState = await issue.state;
-    tasks[issue.id] = {title, priority, state: workflowState?.name};
-  }
-
-  // if (myIssues.nodes.length) {
-  //   logger.info("myIssues.nodes.length", myIssues.nodes.length);
-  //   await Promise.all(myIssues.nodes.map(
-  //     async (issue) => {
-  //       logger.info("issue", issue);
-  //       const state = await issue.state;
-  //       logger.info("state", state);
-  //       // tasks.push({title: issue.title, state});
-  //     }
-  //   ));
-  // }
-  const payload = {tasks};
-  logger.info("getTasks", payload);
-  return payload;
+  const docRef = db.collection("issues").doc(id);
+  await docRef.set(issue, {merge: true});
+  res.send(200);
 });
