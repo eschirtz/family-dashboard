dd5148538114393d3130ba1b2f0f7d8a79806716
Eric Schirtzinger <eschirtzinger@gmail.com>
2023-12-27 11:39:03 -0600

    timestamps baby

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,18 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+import {getFirestore, Timestamp} from "firebase-admin/firestore";
 
 initializeApp();
 const db = getFirestore();
+
+function toTimestamp(strDate?: string): Timestamp | null {
+  if (!strDate) {
+    return null;
+  }
+  const datum = new Date(strDate);
+  return Timestamp.fromDate(datum);
+}
 
 export const linearIssues = onRequest(async (req, res) => {
   logger.info("issue changed");
@@ -15,6 +23,14 @@ export const linearIssues = onRequest(async (req, res) => {
   logger.info("id", id);
 
   const docRef = db.collection("issues").doc(id);
+
+  try {
+    issue.timeCreated = toTimestamp(issue.createdAt);
+    issue.timeUpdated = toTimestamp(issue.updatedAt);
+    issue.timeCompleted = toTimestamp(issue.completedAt);
+  } catch (e) {
+    logger.error("Timestamp conversion", e);
+  }
 
   // TODO: Convert timestamps to Firestore Timestamps so they can be queried
   await docRef.set(issue, {merge: true});
